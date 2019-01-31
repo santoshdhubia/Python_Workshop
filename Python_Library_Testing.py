ea45a60e42d6d56efae90bb98c803c132ca1475a
Santosh Dhubia <37169367+santoshdhubia@users.noreply.github.com>
2019-01-31 11:30:07 +0530

    update library testing

---
+++ Python_Library_Testing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a script file to test required library for Python Workshop.
+"""
+import numpy as np
+import matplotlib
+import pylab
+import matplotlib.pyplot
+import scipy.optimize as optimize
+print('Library Testing Successful')
